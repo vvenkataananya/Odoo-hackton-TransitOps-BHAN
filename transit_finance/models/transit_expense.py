735d2f880322aa4610439d3e01656f238a3be070
bhargav19-amarthaluru <bhargavamarthaluru115@gmial.com>
2026-07-12 15:01:55 +0530

    [Add] Enhanced version of the finance module, implemented the new futures in the finance

--- transit_finance/models/transit_expense.py
+++ transit_finance/models/transit_expense.py
@@ -1,9 +1,10 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class TransitExpense(models.Model):
     _name = 'transit.expense'
     _description = 'Transit Expense'
+    _inherit = ['mail.thread']
     _order = 'date desc, id desc'
 
     name = fields.Char(required=True, string="Expense Description")
@@ -19,7 +20,7 @@ class TransitExpense(models.Model):
         ('other', 'Other'),
     ], required=True, default='other')
     amount = fields.Float(required=True, help="Expense amount")
-    date = fields.Date(required=True, default=fields.Date.context_today)
+    date = fields.Date(required=True, default=lambda self: fields.Date.context_today(self))
     payment_method = fields.Selection([
         ('cash', 'Cash'),
         ('card', 'Credit/Debit Card'),
@@ -37,3 +38,30 @@ class TransitExpense(models.Model):
     ], default='draft', required=True, tracking=True)
     approved_by = fields.Many2one('res.users', string="Approved By", readonly=True)
     approval_date = fields.Datetime(readonly=True)
+
+    def action_approve(self):
+        for rec in self:
+            if rec.state != 'draft':
+                continue
+            rec.write({
+                'state': 'approved',
+                'approved_by': self.env.user.id,
+                'approval_date': fields.Datetime.now(),
+            })
+
+    def action_reject(self):
+        for rec in self:
+            if rec.state != 'draft':
+                continue
+            rec.write({
+                'state': 'rejected',
+                'approved_by': self.env.user.id,
+                'approval_date': fields.Datetime.now(),
+            })
+
+    def action_reset_draft(self):
+        self.filtered(lambda r: r.state in ('approved', 'rejected')).write({
+            'state': 'draft',
+            'approved_by': False,
+            'approval_date': False,
+        })
