730becad78140843e8bc684cdb4846ff647bc045
vvenkataananya <vvenkataananya@gmail.com>
2026-07-12 14:57:00 +0530

    [Add] vehicle and maintenance improved.

--- transit_fleet/models/transit_maintenance.py
+++ transit_fleet/models/transit_maintenance.py
@@ -1,9 +1,11 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 
 class TransitMaintenance(models.Model):
     _name = 'transit.maintenance'
     _description = 'Vehicle Maintenance Record'
+    _inherit = ['mail.thread']
     _order = 'start_date desc'
 
     name = fields.Char(required=True, string="Maintenance Title")
@@ -31,3 +33,32 @@ class TransitMaintenance(models.Model):
     mechanic = fields.Char(help="Name of mechanic or service center")
     company_id = fields.Many2one('res.company', default=lambda self: self.env.company)
     note = fields.Text()
+
+    @api.constrains('end_date', 'start_date')
+    def _check_dates(self):
+        for rec in self:
+            if rec.end_date and rec.start_date and rec.end_date < rec.start_date:
+                raise ValidationError("End date cannot be before start date!")
+
+    def action_start(self):
+        for rec in self:
+            if rec.vehicle_id.status == 'retired':
+                raise ValidationError("Cannot start maintenance on a retired vehicle!")
+            rec.write({'state': 'in_progress'})
+            rec.vehicle_id.write({'status': 'in_shop'})
+
+    def action_close(self):
+        for rec in self:
+            if not rec.end_date:
+                rec.end_date = fields.Datetime.now()
+            rec.write({'state': 'done'})
+            rec.vehicle_id.write({'status': 'available'})
+
+    def action_cancel(self):
+        for rec in self:
+            in_progress = rec.filtered(lambda r: r.state == 'in_progress')
+            in_progress.mapped('vehicle_id').write({'status': 'available'})
+            rec.write({'state': 'cancelled'})
+
+    def action_reset_draft(self):
+        self.filtered(lambda r: r.state == 'cancelled').write({'state': 'draft'})
